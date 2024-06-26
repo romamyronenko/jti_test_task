a02eb893673936a4c585084d61f95d1dfc43be7a
romamyronenko123 <eoma575@gmail.com>
2024-06-26 18:46:12 +0300

    create objects for event

--- event_manager/event_manager/urls.py
+++ event_manager/event_manager/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from event_rest.views import EventViewSet
+
+router = DefaultRouter()
+router.register('event_rest', EventViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
